be8940b4ba1fde76e8c605204ee1976ea27ba629
Nick <mojiotok167@gmail.com>
2022-09-05 22:58:02 +0300

    выполнил домашнее задание 6

---
+++ 2S_HW/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число для первого варианта через пробел"); 
+
+// Первый вариант (мой с использованием массива):
+
+string [] i = Console.ReadLine().Split(' '); // в терминале вводится строковый массив, через пробел. Который разбивается на части
+int index = 0;
+int [] array = new int[i.Length]; // создание целочисленного массива, с тем же размером что и строковый
+
+for (index = 0; index < i.Length; index++) // цикл для заполнения пустого целочисленного массива
+    array [index] = Convert.ToInt32(i[index]); // строковый массив преобразуем в целочисленный
+
+Console.WriteLine(" ");
+Console.WriteLine("Первый вариант");
+
+if (i.Length > 2) // проверка что третья цифра в числе есть
+{
+    Console.WriteLine(array[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+// Второй вариант, тоже с массивом, но я его подглядел в интернете. Он значительно проще моего варианта
+
+Console.WriteLine(" ");
+Console.WriteLine("Введите число для второго варианта без пробела"); 
+int Number1 = Convert.ToInt32(Console.ReadLine()); // запись числа в переменную
+string StringNumber1 = Convert.ToString(Number1); // преобразование переменной в строковый массив
+
+Console.WriteLine(" ");
+Console.WriteLine("Второй вариант");
+
+if (StringNumber1.Length > 2)
+{
+    Console.WriteLine("Третья цифра равна " + StringNumber1[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+Console.WriteLine(" ");
---
+++ 2S_HW/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число "); 
+int number1 = int.Parse(Console.ReadLine());
+//int number3_1 = Convert.ToInt32(Console.ReadLine()); 
+
+// первый способ:
+
+int a1 = number1 % 100;
+int a2 = a1 / 10;
+
+Console.WriteLine(" "); // пропуск строчки после ввода числа
+Console.WriteLine("Первый способ:");
+Console.WriteLine($"{number1} -> {a2}");
+
+// Второй способ (модификация классной задачи):
+
+int a3 = number1 % 10;
+int a4 = number1 / 100;
+
+Console.WriteLine(" "); // Пропуск строчки после вывода первого варианта
+Console.WriteLine("Второй способ:");
+Console.WriteLine($"{number1} -> {(number1 - (a4 * 100) - a3)/10}");
+Console.WriteLine(" "); // Пропуск строчки после вывода второго варианта
+
+// Третий способ (с помощью массива):
+
+string StringNumber = Convert.ToString(number1);
+Console.WriteLine("Третий способ:");
+Console.WriteLine($"{number1} -> {StringNumber[1]}");
+Console.WriteLine(" "); // Пропуск строчки после вывода третьего варианта
+
