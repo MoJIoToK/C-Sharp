8b363a34218d19cf7a044719181a040b5ee93d8c
Nick <mojiotok167@gmail.com>
2022-09-18 14:17:16 +0300

    Добавил домашнее задание к 6 семинару

---
+++ 6S_HW/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Beep();
+Console.Clear();
+
+int [] FillArray(string[] strArr)
+{
+    int count = 0;
+    int [] Num = new int[strArr.Length];
+    foreach (string el in strArr)
+    {
+        Num[count] = int.Parse(el);
+        count++;
+    }
+    return Num;
+}
+
+int GetProcess(int[] arr)
+{
+    int i = 0;
+    int num = 0;
+    foreach (int el in arr)
+    {
+        if (arr[i] > 0) num++;
+        i++;
+    }
+    return num;
+}
+
+Console.WriteLine("Введите числа: ");
+string[] strNumbers = Console.ReadLine().Split(',');
+
+int[] intNumber = FillArray(strNumbers);
+Console.WriteLine($"{String.Join(", ", intNumber)}  ->  {GetProcess(intNumber)}");
+
---
+++ 6S_HW/Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Beep();
+Console.Clear();
+
+Console.WriteLine("Первая прямая y=k1*x+b1. Введите k1 и b1 через запятую: ");
+string[] strLine1 = Console.ReadLine().Split(',');
+
+Console.WriteLine("Вторая прямая y=k2*x+b2. Введите k2 и b2 через запятую: ");
+string[] strLine2 = Console.ReadLine().Split(',');
+
+double[] dLine1 = FillArray(strLine1);
+double[] dLine2 = FillArray(strLine2);
+
+double[] resP = new double[2];
+
+bool test = TestOfIntersection(dLine1, dLine2);
+
+if (test == true)
+    resP = GetProcess(dLine1, dLine2);
+else Console.WriteLine("Линии не пересекаются");
+
+Console.WriteLine($"Первая прямая - ({String.Join(", ", dLine1)}), Вторая прямая - ({String.Join(", ", dLine2)}) -> ({String.Join("; ", resP)})");
+
+Console.WriteLine();
+
+Console.WriteLine($"b1 = {dLine1[1]}, k1 = {dLine1[0]}, b2 = {dLine2[1]}, k2 = {dLine2[0]}  -> ({String.Join("; ", resP)})");
+
+double[] FillArray(string[] strArr)
+{
+    int count = 0;
+    double[] Num = new double[strArr.Length];
+    foreach (string el in strArr)
+    {
+        Num[count] = double.Parse(el);
+        count++;
+    }
+    return Num;
+}
+
+bool TestOfIntersection(double[] arr1, double[] arr2)
+{
+    return dLine1[0] != dLine2[0];
+}
+
+double[] GetProcess(double[] arr1, double[] arr2)
+{
+    double[] resPoint = new double[2];
+    resPoint[0] = (dLine2[1] - dLine1[1]) / (dLine1[0] - dLine2[0]);
+    resPoint[1] = (dLine1[0] * resPoint[0]) + (dLine1[1]);
+    //    Console.Write($" -> {String.Join("; ", resPoint)}");
+    return resPoint;
+}
