3165b5f9f04681f99b09dcf26241a16300d964e6
Nick <mojiotok167@gmail.com>
2022-09-25 13:04:55 +0300

    Добавил седьмую лекцию

---
+++ 7L_CW/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿string path = "C:/Users/Nick/Desktop/Коля/Geekbrains"; // ужно при копировании знак "\" заменить на "/"
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
---
+++ 7L_CW/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = @"C:/Users/Nick/Desktop/Коля/Geekbrains/Введение в программирование";
+CatalogInfo(path);
+
+//     DirectoryInfo catalogs = new DirectoryInfo(path);
+//     foreach (var currentCatalog in catalogs.GetDirectories())
+//     {
+//         Console.WriteLine($"{indent}{currentCatalog.Name}");
+//         CatalogInfo(currentCatalog.FullName, indent + " ");
+//     }
+//     foreach (var item in catalogs.GetFiles())
+//     {
+//         Console.WriteLine($"{indent}{item.Name}");
+//     }
+// }
+// string path = @"/Users/sergejkamaneckij/Projects/HelloCode";
+// CatalogInfo(path);
