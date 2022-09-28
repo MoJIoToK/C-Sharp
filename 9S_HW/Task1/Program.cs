b27d12cf26d2d07d427750fd41f69abd1133e668
Nick <mojiotok167@gmail.com>
2022-09-28 23:12:52 +0300

    Добавил первые две задачи к девятому семинару

---
+++ 9S_HW/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+Console.Clear();
+
+int cons = 10;
+Output(cons);
+
+int Output(int constanta)
+{
+    if (constanta < 2) return 0;
+    else
+        if (constanta % 2 == 0) Console.Write($"{constanta}, ");
+    return Output(constanta - 1);
+}
+
+
+// int cons = 5;
+// Output(1, cons);
+
+// void Output (int num, int constanta)
+// {
+//     if (num == constanta+1) return;
+//     if (num % 2 == 0) Console.Write($"{num}, ");
+//     Output(num+1, constanta);
+// }
---
+++ 9S_HW/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.Write("Введите нижнюю границу диапазона: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите верхнюю границу диапазона: ");
+int n = int.Parse(Console.ReadLine());
+int summa = 0;
+
+if (n > m) PrintSum(m, n, summa);
+else
+{
+    Console.WriteLine("Введены неправильные границы диапазона, но не переживайте, я это учел!");
+    PrintSum(n, m, summa);
+}
+
+void PrintSum(int min, int max, int sum)
+{
+    if (min <= max)
+    {
+        sum = sum + min;
+    }
+    else
+    {
+        Console.Write($"{sum}");
+        return;
+    }
+    PrintSum(min + 1, max, sum);
+}
+
+// void PrintSum(int min, int max, int sum)
+// {
+//     sum = sum + min;
+//     if (min > max-1)
+//     {
+//         Console.Write($"{sum}"); 
+//         return;
+//     }
+//     PrintSum(min + 1, max, sum);
+// }
