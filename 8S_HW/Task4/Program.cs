a019aba922f72764df098224c2e2cefafd9ace38
Nick <mojiotok167@gmail.com>
2022-09-28 22:03:24 +0300

    Добавил вторую часть  дз к восьмому семинару

--- 8S_HW/Task4/Program.cs
+++ 8S_HW/Task4/Program.cs
@@ -6,4 +6,48 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+Console.Clear();
+
+int axisX = 2;
+int axisY = 2;
+int axisZ = 2;
+
+
+int[,,] array = GetArray(axisX, axisY, axisZ, 10, 100);
+PrintArray(array);
+
+int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
+{
+    int[,,] result = new int[x, y, z];
+    int r = new Random().Next(-minValue, maxValue);
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                result[i, j, k] = r + (new Random().Next(minValue, maxValue + 1));
+            }
+
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($" {inArray[i, j, k]} ({i},{j},{k})");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
 //https://github.com/Herobrina2306/Dz8
--- 8S_HW/Task5/Program.cs
+++ 8S_HW/Task5/Program.cs
@@ -4,3 +4,40 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+Console.Beep();
+Console.Clear();
+
+int n = 4;
+int[,] array = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= n * n)
+{
+    array[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < array.GetLength(1) - 1) j++; // проходит нулевую строку слева направо
+    else if (i < j && i + j >= array.GetLength(0) - 1) i++; // проходит правый столбец сверху вниз
+    else if (i >= j && i + j > array.GetLength(1) - 1) j--; // проходит последнюю строку справо налево
+    else i--;// проходит левый столбец снизу вверх
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($"0{array[i, j]}  ");
+
+            else Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
